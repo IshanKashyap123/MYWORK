14204760ef791619d6b04088c11e9dd6ea84f45d
IshanKashyap123 <ishan44884488@gmail.com>
2025-06-18 13:14:48 +0530

    Update urls.py

--- bikeaccessories/urls.py
+++ bikeaccessories/urls.py
@@ -29,3 +29,7 @@ urlpatterns = [
     path('cart/',views.cart_func, name='cart_func'),
    
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
